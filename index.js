b6ffee6edf9e3236a8fa4651b928ea02abe01cb6
Jessy Kandola <kandolajs@gmail.com>
2017-08-09 15:02:03 -0400

    triblio and mailinator domains that are free

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var fs = require('fs');
 var tldjs = require('tldjs');
+var crypto = require("crypto");
 
 var disposable = fs.readFileSync(__dirname + '/data/disposable.txt').toString().split('\n');
 var free = fs.readFileSync(__dirname + '/data/free.txt').toString().split('\n').concat(disposable);
+var withTrDomains = fs.readFileSync(__dirname +"/data/triblio_added.txt").toString().split("\n").concat(free);
+var sha1Domains = fs.readFileSync(__dirname+ "/data/mailinator_sha1.txt").toString().split("\n");
+
 
 function isFree(email) {
     if (typeof email !== 'string') throw new TypeError('email must be a string');
     var domain = tldjs.getDomain(email.split('@').pop());
-    return free.indexOf(domain) !== -1;
+    return (withTrDomains.indexOf(domain) !== -1 || _isInShaDomains(domain));
 }
 
 function isDisposable(email) {
@@ -20,3 +24,14 @@ module.exports = {
     isFree: isFree,
     isDisposable: isDisposable
 };
+
+
+function _isInShaDomains(domain){
+  return sha1Domains.indexOf(sha1(domain)) !== -1;
+}
+
+function sha1(string){
+  var shasum = crypto.createHash('sha1');
+  shasum.update(string);
+  return shasum.digest('hex');
+}
